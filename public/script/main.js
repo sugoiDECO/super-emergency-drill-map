69fb774d2759441416da479ab631ad97fec8ec20
Kenyat1989 <erdkunde.1989@mapconcierge.com>
2016-01-24 03:01:13 +0900

    console.logの追加 part2

--- public/script/main.js
+++ public/script/main.js
@@ -32,6 +32,7 @@ $(window).load(function() {
 
   map = L.map('map', {zoomControl: false}).setView([34.6042334, 135.5530289], 5);
   console.log(map);
+  console.log("Hollo World");
 
   //map.on('popupopen', function(e) {
   //  console.log('popupopen');
